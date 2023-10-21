6439b9ff8a4e7ce2a6dde3e2c940f0c4a360671f
isichei <karik.isichei@gmail.com>
2023-10-21 15:21:51 +0100

    Adding some git commands

--- after/plugin/defaults.lua
+++ after/plugin/defaults.lua
@@ -28,3 +28,6 @@ end)
 
 vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gc<Left><Left><Left>]])
 vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true })
+
+vim.keymap.set("n", "<leader>tb", "<cmd>Gitsigns toggle_current_line_blame<CR>")
+vim.keymap.set("n", "<leader>hd", "<cmd>Gitsigns diffthis<CR>")
