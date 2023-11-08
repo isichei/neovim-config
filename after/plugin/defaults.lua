f92ec40fc754b2622ab6cc8b31b0110106c5a085
karik isichei <karik.isichei@genomicsengland.co.uk>
2023-11-08 23:32:27 +0000

    Fixing some clashing bindings

--- after/plugin/defaults.lua
+++ after/plugin/defaults.lua
@@ -14,13 +14,10 @@ vim.o.scrolloff = 8
 
 -- Key remaps
 vim.keymap.set("v", "<leader>p", "\"_dP", { remap = true })
-vim.keymap.set("n", "<leader>t", "<Cmd>Neotree toggle<CR>", { remap = true })
+vim.keymap.set("n", "<leader>nt", "<Cmd>Neotree toggle<CR>", { remap = true })
 
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
-
-vim.keymap.set("n", "n", "nzzzv")
-vim.keymap.set("n", "N", "Nzzzv")
 
 vim.keymap.set("n", "<leader>f", function()
 	vim.lsp.buf.format()
@@ -30,4 +27,4 @@ vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gc<Left><Left><
 vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true })
 
 vim.keymap.set("n", "<leader>tb", "<cmd>Gitsigns toggle_current_line_blame<CR>")
-vim.keymap.set("n", "<leader>hd", "<cmd>Gitsigns diffthis<CR>")
+vim.keymap.set("n", "<leader>vd", "<cmd>Gitsigns diffthis<CR>")
